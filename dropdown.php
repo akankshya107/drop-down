083f36717fe4a856e1f820607b7cad3cbcaa6a55
akankshya107 <akankshyam@gmail.com>
2018-06-27 19:09:54 +0530

    State and city

--- dropdown.php
+++ dropdown.php
@@ -1,27 +1,35 @@
 <?php
 #the values for the dropdown box
+/**
+ * @file
+ * Contains \ircs\drop-down\Form\DropDownController.
+ */
+namespace ircs\dropdown\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
 //address of host
-$host= "";	
-//username sql					 
-$username= "";
-//password of sql 								
-$password= ""; 								
+$host= "localhost";
+//username sql
+$username= "admin";
+//password of sql
+$password= "ircsrocks@420";
 $connection - mysql_connect($host,$username,$password)
 or die{mysql_error()} ;
 
 //name of database used
-$db_name = "";	
-//choosing databasese							
+$db_name = "ircs";
+//choosing databasese
 mysql_select_db(db_name,$connection);
 //name of the tale to be used
 $table_name = "";
 //Select states from dbs
-$sql_q = "select state from branch;		
+$sql_q1 = "select state from $table_name";
 
-$result = mysql_query($sql_q, $connection);
+$result1 = mysql_query($sql_q1, $connection);
 
 $types = array();
-while(($row =  mysql_fetch_assoc($result))) {
+while(($row =  mysql_fetch_assoc($result1))) {
 	$types[] = $row['type'];
 }
 
@@ -37,3 +45,28 @@ $form['type'] = array(
 	'#options' => $form['type_options']['#value']
 );
 
+// Check if the form is submitted
+if ( isset( $_POST['submit'] ) ) {
+	$state = $_REQUEST['state'];
+}
+
+//sql query for selecting cities from database
+$sql_q2 = "select city from $table_name where state = $state";		
+
+$result2 = mysql_query($sql_q2, $connection);
+$types = array();
+while(($row =  mysql_fetch_assoc($result1))) {
+	$types[] = $row['type'];
+}
+
+$form['type_options'] = array(
+	'#type' =>'value',
+	'#value' => $types
+);
+
+$form['type'] = array(
+	'#title' => t('City'),
+	'#type' => 'select',
+	'#description' => "Select the city",
+	'#options' => $form['type_options']['#value']
+);
