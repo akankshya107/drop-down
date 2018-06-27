cca48794e3993dc2fb03e50be0e7d4b9308df655
akankshya107 <akankshyam@gmail.com>
2018-06-27 13:08:33 +0530

    For fetching state

--- dropdown.php
+++ dropdown.php
@@ -1,9 +1,30 @@
 <?php
 #the values for the dropdown box
+//address of host
+$host= "";	
+//username sql					 
+$username= "";
+//password of sql 								
+$password= ""; 								
+$connection - mysql_connect($host,$username,$password)
+or die{mysql_error()} ;
+
+//name of database used
+$db_name = "";	
+//choosing databasese							
+mysql_select_db(db_name,$connection);
+//name of the tale to be used
+$table_name = "";
+//Select states from dbs
+$sql_q = "select state from branch;		
+
+$result = mysql_query($sql_q, $connection);
+
 $types = array();
 while(($row =  mysql_fetch_assoc($result))) {
 	$types[] = $row['type'];
 }
+
 $form['type_options'] = array(
 	'#type' =>'value',
 	'#value' => $types
@@ -15,3 +36,4 @@ $form['type'] = array(
 	'#description' => "Select the state",
 	'#options' => $form['type_options']['#value']
 );
+
